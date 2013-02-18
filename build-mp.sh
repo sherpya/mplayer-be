c9fc31b020054060cd611588b4ef5a6630638168
sherpya <sherpya@netfarm.it>
2013-02-18 09:00:23 +0100

    builds: changed build order of mplayer versions

--- build-mp.sh
+++ build-mp.sh
@@ -2,10 +2,11 @@
 tool=$(dirname $0)/mp.sh
 
 $tool build_source
-$tool build_binary generic
+
+HOST=x86_64-w64-mingw32 $tool build_binary x86_64
+
 $tool build_binary core2
 $tool build_binary corei7
 $tool build_binary k8-sse3
 $tool build_binary bdver1
-
-HOST=x86_64-w64-mingw32 $tool build_binary x86_64
+$tool build_binary generic
