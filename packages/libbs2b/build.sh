1016a965dd2a0163507849885e0f053a4e8a9d49
sherpya <sherpya@netfarm.it>
2013-03-10 06:07:38 +0100

    packages: fixed libbs2b build on x86_64

--- packages/libbs2b/build.sh
+++ packages/libbs2b/build.sh
@@ -8,4 +8,8 @@ BASEURL=http://downloads.sourceforge.net/project/bs2b/libbs2b/3.1.0
 
 STATICLIBS="libbs2b"
 
+# not cross build friendly
+export ac_cv_func_malloc_0_nonnull=yes
+export ac_cv_header_stdc=yes
+
 pkg_build && pkg_clean
