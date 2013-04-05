cd072e299e079667b4fad885aae966f41eba2eb8
sherpya <sherpya@netfarm.it>
2013-04-06 01:39:27 +0200

    packages: added docs/license to frei0r bindist

--- packages/frei0r/build.sh
+++ packages/frei0r/build.sh
@@ -11,10 +11,15 @@ CMAKEOPTS="-DCMAKE_MODULE_LINKER_FLAGS=-s"
 
 git_clean && pkg_build && git_clean
 
+# LIBDIR becames the binary package
 LIBDIR=${PREFIX}/lib/frei0r-1
 
 chmod 644 ${LIBDIR}/*.dll
 for sharedlib in libgcc_s_sjlj-1.dll libstdc++-6.dll; do
     install -v -m644 $(print_file_name $sharedlib) ${LIBDIR}/$sharedlib
     ${CROSS_PREFIX}strip ${LIBDIR}/$sharedlib
+done
+
+for doc in AUTHORS ChangeLog COPYING README; do
+    unix2dos -n ${BUILDDIR}/$doc ${LIBDIR}/$doc.txt
 done
