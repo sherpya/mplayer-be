8ff634e703fb184b4b864e789ea9ac6aac20333d
sherpya <sherpya@netfarm.it>
2013-06-03 05:32:15 +0200

    builds: disabled bdver1 build

--- build-mp.sh
+++ build-mp.sh
@@ -12,5 +12,5 @@ HOST=x86_64-w64-mingw32 $tool build_binary x86_64
 $tool build_binary core2
 $tool build_binary corei7
 $tool build_binary k8-sse3
-$tool build_binary bdver1
+#$tool build_binary bdver1
 $tool build_binary generic
