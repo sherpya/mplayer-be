55ab90bd14494d74dcb794754300366e86b88ace
sherpya <sherpya@netfarm.it>
2013-06-26 00:17:13 +0200

    builds: updated debian patch/setup for 3.0~svn5915-1

--- packages/setup.sh
+++ packages/setup.sh
@@ -10,6 +10,8 @@ for dir in bin lib include live; do
     sudo chown $owner ${PREFIX}/$dir
 done
 
+sudo rm -f ${PREFIX}/lib/lib*pthread*
+
 PKGCONFIG="${PREFIX}/bin/${HOST}-pkg-config"
 
 cat > ${PKGCONFIG} << EOF
