ba9994f81ee79ad8fe98062d69d777a25fd913f4
sherpya <sherpya@netfarm.it>
2014-06-08 05:48:57 +0200

    build: force stop if binfmt support is enabled

--- build-mp.sh
+++ build-mp.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # MPlayer/FFmpeg MinGW-w64 Cross Build Environment
-# Copyright (c) 2013 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2014 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
+
+grep -q enabled /proc/sys/fs/binfmt_misc/status 2>/dev/null && \
+ { echo "binfmt support is enabled, aborting"; exit 1; }
 
 tool=$(dirname $0)/mp.sh
 
