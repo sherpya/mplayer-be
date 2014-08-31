22a73c4ef8daef05144931e6f4bb589dbdbbf71b
sherpya <sherpya@netfarm.it>
2014-08-31 04:13:57 +0200

    packages: updated libpng

--- packages/libpng/build.sh
+++ packages/libpng/build.sh
@@ -4,7 +4,7 @@
 # See LICENSE for licensing informations
 
 PACKAGE=libpng
-VERSION=1.6.12
+VERSION=1.6.13
 EXT=tar.xz
 BASEURL=http://downloads.sourceforge.net/project/${PACKAGE}/libpng16/${VERSION}
 
