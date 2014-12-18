6fbf5bf03e5869740bb4082da309785b1cfc8cf2
sherpya <sherpya@netfarm.it>
2014-12-18 06:00:53 +0100

    packages: force dependency on pthread to ilbc

--- packages/libilbc/build.sh
+++ packages/libilbc/build.sh
@@ -10,4 +10,12 @@ GIT_REPO="git://github.com/dekkers/libilbc.git"
 BUILDDIR=libilbc
 STATICLIBS="libilbc"
 
+pkg_make_target()
+{
+    make ${MAKEOPTS} install
+    # add missing pthread dependency
+    sed -i -e "s,dependency_libs='',dependency_libs='-lpthread',g" ${PREFIX}/lib/libilbc.la
+}
+
+
 git_clean && pkg_build && git_clean
