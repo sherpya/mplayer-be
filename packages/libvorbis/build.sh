f4eb9d78ed68c15ae6c8763ff162cbfeb1d2aae8
sherpya <sherpya@netfarm.it>
2015-03-25 04:28:19 +0100

    packages: updated libvorbis to 1.3.5

--- packages/libvorbis/build.sh
+++ packages/libvorbis/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Build script for Ogg Vorbis Audio Codec
-# Copyright (c) 2013-2014 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2015 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 # includes fixes for: CVE-2012-0444
 
 PACKAGE=libvorbis
-VERSION=1.3.4
+VERSION=1.3.5
 EXT=tar.xz
 BASEURL=http://downloads.xiph.org/releases/vorbis
 
