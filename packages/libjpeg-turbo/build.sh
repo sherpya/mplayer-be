b2f1fe56381adc1e48c5a2212bb2d6a6850c7962
sherpya <sherpya@netfarm.it>
2015-07-30 07:26:04 +0200

    packages: updated libjpeg-turbo to 1.4.1

--- packages/libjpeg-turbo/build.sh
+++ packages/libjpeg-turbo/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Build script for libjpeg-turbo
-# Copyright (c) 2013-2014 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2015 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 # includes fixes for: CVE-2013-6629, CVE-2013-6630
 
 PACKAGE=libjpeg-turbo
-VERSION=1.4.0
+VERSION=1.4.1
 EXT=tar.gz
 BASEURL=http://downloads.sourceforge.net/project/${PACKAGE}/${VERSION}
 
