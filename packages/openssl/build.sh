76a5dc084af4459ef127d522e10106d5279a5ab0
sherpya <sherpya@netfarm.it>
2015-08-22 03:57:07 +0200

    packages: added openssl (disabled)

---
+++ packages/openssl/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Build script for openssl library
+# Copyright (c) 2015 Gianluigi Tiesi <sherpya@netfarm.it>
+# See LICENSE for licensing informations
+
+PACKAGE=openssl
+VERSION=1.0.2d
+EXT=tar.gz
+BASEURL=https://www.openssl.org/source
+
+. $(dirname $0)/../functions.sh
+
+case ${ARCH} in
+    x86) OS=mingw ;;
+    x86_64) OS=mingw64 ;;
+esac
+
+#STATICLIBS="libpng libpng16"
+
+pkg_configure()
+{
+    CROSS_COMPILE="${HOST}-" ./Configure ${OS} \
+    no-shared no-dso \
+    --prefix=${PREFIX}
+}
+
+pkg_make_target()
+{
+    make ${MAKEOPTS} || return 1
+    make install_sw || return 1
+}
+
+pkg_build && pkg_clean
