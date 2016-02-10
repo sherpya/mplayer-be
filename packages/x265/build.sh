d116111b0164f741318f84137ece4f917cf637f7
sherpya <sherpya@netfarm.it>
2016-02-10 02:21:31 +0100

    packages: x265: enabled winxp/vista compatibility (x86)

--- packages/x265/build.sh
+++ packages/x265/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Build script for x265 H.265/MPEG-H HEVC Encoder
-# Copyright (c) 2014 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2014-2016 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 HG_REPO="http://hg.videolan.org/x265"
@@ -14,5 +14,8 @@ CMAKE=1
 CMAKEOPTS=" \
     -DENABLE_SHARED=0 \
     -DENABLE_CLI=0"
+
+# enable windows xp/vista compatibility in x86 build
+[ "${ARCH}" = "x86" ] && CMAKEOPTS+=" -DWINXP_SUPPORT=1"
 
 hg_clean && pkg_build && hg_clean
