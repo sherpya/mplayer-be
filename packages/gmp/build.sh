4a215dc0e3db3ff5855a26fd6c3ef6c356333bfc
sherpya <sherpya@netfarm.it>
2016-02-26 02:28:43 +0100

    packages: force 486 instruction set when building gmp x86

--- packages/gmp/build.sh
+++ packages/gmp/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Build script for GNU GMP library
-# Copyright (c) 2015 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2015-2016 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=gmp
@@ -9,6 +9,9 @@ EXT=tar.xz
 BASEURL=https://gmplib.org/download/${PACKAGE}
 
 . $(dirname $0)/../functions.sh
+
+# force i486 instruction set
+[ "${ARCH}" = "x86" ] && export MPN_PATH="x86/i486 x86 generic"
 
 BUILDDIR=${PACKAGE}-6.0.0
 
