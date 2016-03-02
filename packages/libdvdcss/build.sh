a78e401eb6142341a795cf26f356dd73d251617e
sherpya <sherpya@netfarm.it>
2016-03-02 03:16:19 +0100

    packages: libdvdcss, use win32 64bit handles on win64

--- packages/libdvdcss/build.sh
+++ packages/libdvdcss/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Build script for libdvdcss
-# Copyright (c) 2014 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2014-2016 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 GIT_REPO="git://git.videolan.org/libdvdcss.git"
@@ -9,7 +9,7 @@ GIT_REPO="git://git.videolan.org/libdvdcss.git"
 
 BUILDDIR=libdvdcss
 STATICLIBS="libdvdcss"
-CFLAGS="-Wno-error=pointer-to-int-cast"
+
 CONFOPTS="--disable-doc"
 
 git_clean && pkg_build && git_clean
