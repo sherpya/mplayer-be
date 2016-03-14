68112f069f1c06d178e94fc42fb5773da1c0930e
sherpya <sherpya@netfarm.it>
2016-03-14 03:01:38 +0100

    packages: updated expat and watchfile

--- packages/expat/build.sh
+++ packages/expat/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Build script for Expat XML Parser
-# Copyright (c) 2013 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2016 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=expat
-VERSION=2.1.0
-EXT=tar.gz
+VERSION=2.1.1
+EXT=tar.bz2
 BASEURL=http://downloads.sourceforge.net/project/${PACKAGE}/${PACKAGE}/${VERSION}
 
 . $(dirname $0)/../functions.sh
