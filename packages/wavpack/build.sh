23fba1e4263cd9961703cfc1274ca59f041cace4
sherpya <sherpya@netfarm.it>
2016-04-07 04:13:47 +0200

    packages: updated wavapack to 4.80.0

--- packages/wavpack/build.sh
+++ packages/wavpack/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Build script for WavPack
-# Copyright (c) 2014-2015 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2014-2016 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=wavpack
-VERSION=4.75.2
+VERSION=4.80.0
 EXT=tar.bz2
 BASEURL=http://wavpack.com
 
