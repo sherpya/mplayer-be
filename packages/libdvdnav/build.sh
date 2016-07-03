bd83c4d7178f8a4eece057be1bf5fa0868e20b97
sherpya <sherpya@netfarm.it>
2016-07-03 08:10:48 +0200

    packages: updated videolan repository urls

--- packages/libdvdnav/build.sh
+++ packages/libdvdnav/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Build script for libdvdnav library
-# Copyright (c) 2014 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2014-2016 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
-GIT_REPO="git://git.videolan.org/libdvdnav.git"
+GIT_REPO="http://code.videolan.org/videolan/libdvdnav.git"
 
 . $(dirname $0)/../functions.sh
 
