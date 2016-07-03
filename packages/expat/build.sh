6e646c64dfab064578651bbc25795f6bca313b7f
sherpya <sherpya@netfarm.it>
2016-07-03 06:36:39 +0200

    packages: updated expat to 2.2.0

--- packages/expat/build.sh
+++ packages/expat/build.sh
@@ -4,7 +4,7 @@
 # See LICENSE for licensing informations
 
 PACKAGE=expat
-VERSION=2.1.1
+VERSION=2.2.0
 EXT=tar.bz2
 BASEURL=http://downloads.sourceforge.net/project/${PACKAGE}/${PACKAGE}/${VERSION}
 
