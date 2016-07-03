2c18a300d038e5d1f61e531694c199fbb5e87000
sherpya <sherpya@netfarm.it>
2016-07-03 07:02:47 +0200

    packages: updated libxml2 to 2.9.4

--- packages/libxml2/build.sh
+++ packages/libxml2/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Build script for libxml2
-# Copyright (c) 2014-2015 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2014-2016 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 # includes fixes for: CVE-2014-3660 (2.9.3)
 
 PACKAGE=libxml2
-VERSION=2.9.3
+VERSION=2.9.4
 EXT=tar.gz
 BASEURL=ftp://xmlsoft.org/libxml2/
 
@@ -16,6 +16,6 @@ depends lib/libz.a
 depends lib/libiconv.a
 
 STATICLIBS="libxml2"
-CONFOPTS="--with-python=no"
+CONFOPTS="--with-python=no --with-http=no --with-ftp=no"
 
 pkg_build && pkg_clean
