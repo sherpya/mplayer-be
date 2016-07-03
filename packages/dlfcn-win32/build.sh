ea3c0420f5f77e791c92a0ca9f5bbf93f0ebc11d
sherpya <sherpya@netfarm.it>
2016-07-03 07:16:45 +0200

    packages: updated dlfcn-win32, switched to git

--- packages/dlfcn-win32/build.sh
+++ packages/dlfcn-win32/build.sh
@@ -3,25 +3,24 @@
 # Copyright (c) 2013-2016 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
-SVN_REPO="http://dlfcn-win32.googlecode.com/svn/trunk"
+GIT_REPO="https://github.com/dlfcn-win32/dlfcn-win32"
 
 . $(dirname $0)/../functions.sh
 
 BUILDDIR=dlfcn-win32
 
-# why configure does return false?
 pkg_configure()
 {
     ./configure                 \
         --prefix=${PREFIX}      \
         --disable-wine          \
-        --cross-prefix=${CROSS_PREFIX} || return 0
+        --cross-prefix=${CROSS_PREFIX}
 }
 
 pkg_make_target()
 {
-    CFLAGS="${GLOBAL_CFLAGS}" make ${MAKEOPTS}
+    CFLAGS="${GLOBAL_CFLAGS}" make V=1 ${MAKEOPTS}
     make ${MAKEOPTS} install
 }
 
-distclean && svn_clean && pkg_build && distclean && svn_clean
+git_clean && pkg_build && git_clean
