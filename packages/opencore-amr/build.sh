1c9efde07437d1c189d833ac2f4fe0a0248d77e8
sherpya <sherpya@netfarm.it>
2017-04-18 18:10:25 +0200

    packages: updated opencore-amr to 0.1.5

--- packages/opencore-amr/build.sh
+++ packages/opencore-amr/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Build script for OpenCORE AMR-NB and AMR-WB Decoder
-# Copyright (c) 2013 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2017 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=opencore-amr
-VERSION=0.1.3
+VERSION=0.1.5
 EXT=tar.gz
 BASEURL=http://downloads.sourceforge.net/project/${PACKAGE}/${PACKAGE}
 
