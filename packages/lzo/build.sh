3bd789f7bc8e97df3826c0d5329bbc9b35bf2cb4
sherpya <sherpya@netfarm.it>
2017-04-18 18:08:32 +0200

    packages: updated lzo to 2.10

--- packages/lzo/build.sh
+++ packages/lzo/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Build script for LZO real-time data compression library
-# Copyright (c) 2013-2014 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2017 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=lzo
-VERSION=2.09
+VERSION=2.10
 EXT=tar.gz
 BASEURL=http://www.oberhumer.com/opensource/${PACKAGE}/download
 
