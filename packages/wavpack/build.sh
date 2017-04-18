cbedd6bcad60e7bc4948efa071ad341a6258ffa0
sherpya <sherpya@netfarm.it>
2017-04-18 18:18:34 +0200

    packages: updated wavpack 5.1.0

--- packages/wavpack/build.sh
+++ packages/wavpack/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # Build script for WavPack
-# Copyright (c) 2014-2016 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2014-2017 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=wavpack
-VERSION=4.80.0
+VERSION=5.1.0
 EXT=tar.bz2
 BASEURL=http://wavpack.com
 
 . $(dirname $0)/../functions.sh
 
 STATICLIBS="libwavpack"
+CONFOPTS="--disable-apps"
 
 pkg_build && pkg_clean
