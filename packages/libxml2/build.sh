c6779882c1b17934a6edb67ed2139c119995db42
sherpya <sherpya@netfarm.it>
2017-11-11 06:07:25 +0100

    packages: libxml2 -> added security infos

--- packages/libxml2/build.sh
+++ packages/libxml2/build.sh
@@ -3,7 +3,15 @@
 # Copyright (c) 2014-2017 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
-# 2.9.3 includes fixes for: CVE-2014-3660
+# patches for:
+# CVE-2017-7375: 01_debian-prevent-pe.diff
+
+# 2.9.7 includes fixes for:
+# CVE-2016-5131 CVE-2017-0663 CVE-2017-5969
+# CVE-2017-7376 CVE-2017-9047 CVE-2017-9048
+
+# 2.9.3 includes fixes for:
+# CVE-2014-3660
 
 PACKAGE=libxml2
 VERSION=2.9.7
