05e6864691c32666177656ef5ea471ddf6d60a48
sherpya <sherpya@netfarm.it>
2017-11-11 02:15:36 +0100

    packages: updated expat to 2.2.5

--- packages/expat/build.sh
+++ packages/expat/build.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 # Build script for Expat XML Parser
-# Copyright (c) 2013-2016 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2017 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=expat
-VERSION=2.2.0
+VERSION=2.2.5
 EXT=tar.bz2
 BASEURL=http://downloads.sourceforge.net/project/${PACKAGE}/${PACKAGE}/${VERSION}
 
 . $(dirname $0)/../functions.sh
 
 STATICLIBS="libexpat"
+CONFOPTS="--without-docbook"
 
 pkg_make_target()
 {
-    make ${MAKEOPTS} installlib
+    make -C lib ${MAKEOPTS} install
 }
 
 pkg_build && pkg_clean
