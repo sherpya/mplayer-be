4802d744e216cd7714b64b398914d03db1b09313
sherpya <sherpya@netfarm.it>
2018-02-03 02:25:54 +0100

    libjpeg-turbo: new upstream 1.5.3

--- packages/libjpeg-turbo/build.sh
+++ packages/libjpeg-turbo/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Build script for libjpeg-turbo
-# Copyright (c) 2013-2017 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2018 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 # 1.5.1 includes fixes for: CVE-2013-6629, CVE-2013-6630
 
 PACKAGE=libjpeg-turbo
-VERSION=1.5.2
+VERSION=1.5.3
 EXT=tar.gz
 BASEURL=http://downloads.sourceforge.net/project/${PACKAGE}/${VERSION}
 
