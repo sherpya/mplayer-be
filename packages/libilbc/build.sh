27d33a3c7f1617236b651d16a3159ba0b0db8316
sherpya <sherpya@netfarm.it>
2018-02-25 07:30:49 +0100

    packages: libilbc -> use cmake

--- packages/libilbc/build.sh
+++ packages/libilbc/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Build script for Internet Low Bitrate Codec (iLBC) library
-# Copyright (c) 2013-2014 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2018 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 GIT_REPO="git://github.com/dekkers/libilbc.git"
@@ -8,14 +8,8 @@ GIT_REPO="git://github.com/dekkers/libilbc.git"
 . $(dirname $0)/../functions.sh
 
 BUILDDIR=libilbc
-STATICLIBS="libilbc"
 
-pkg_make_target()
-{
-    make ${MAKEOPTS} install
-    # add missing pthread dependency
-    sed -i -e "s,dependency_libs='',dependency_libs='-lpthread',g" ${PREFIX}/lib/libilbc.la
-}
-
+CMAKE=1
+CMAKEOPTS="-DBUILD_SHARED_LIBS=OFF"
 
 git_clean && pkg_build && git_clean
