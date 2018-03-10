403aa919c4b09e3487f9ded7410622b7d61e5d2c
sherpya <sherpya@netfarm.it>
2018-03-10 03:53:41 +0100

    packages: updated fribidi to 1.0.1

--- packages/fribidi/build.sh
+++ packages/fribidi/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Build script for GNU FriBidi
-# Copyright (c) 2013-2015 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2018 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=fribidi
-VERSION=0.19.7
+VERSION=1.0.1
 EXT=tar.bz2
-BASEURL=http://fribidi.org/download
+BASEURL=https://github.com/fribidi/fribidi/releases/download/v${VERSION}
 
 . $(dirname $0)/../functions.sh
 
