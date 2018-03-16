0af343daf6a6df95145e90a9a113ab785f8aeb0a
sherpya <sherpya@netfarm.it>
2018-03-16 02:49:18 +0100

    packages: regex, provided cmakefile

--- packages/regex/build.sh
+++ packages/regex/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Build script for GNU regex
-# Copyright (c) 2013-2016 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2018 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=regex
@@ -10,19 +10,6 @@ BASEURL=http://ftp.gnu.org/old-gnu/${PACKAGE}
 
 . $(dirname $0)/../functions.sh
 
-pkg_configure()
-{
-    : # skip
-}
-
-pkg_make_target()
-{
-    cmd ${CROSS_PREFIX}gcc ${GLOBAL_CFLAGS} -DSTDC_HEADERS=1 -DHAVE_STRING_H=1 -c regex.c -o regex.o
-    cmd ${CROSS_PREFIX}ar cr libregex.a regex.o
-    cmd ${CROSS_PREFIX}ranlib libregex.a
-
-    cmd install -m644 libregex.a ${PREFIX}/lib/libregex.a
-    cmd install -m644 regex.h ${PREFIX}/include/regex.h
-}
+CMAKE=1
 
 pkg_build && pkg_clean
