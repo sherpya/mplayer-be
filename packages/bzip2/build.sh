4b9e1dfa05578574ebdc4f0d54a872ad7bb56003
sherpya <sherpya@netfarm.it>
2018-03-16 04:10:19 +0100

    packages: bzip2 made cmakefile

--- packages/bzip2/build.sh
+++ packages/bzip2/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Build script for bzip2 data compressor
-# Copyright (c) 2013 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2018 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=bzip2
@@ -10,16 +10,6 @@ BASEURL=http://www.bzip.org/${VERSION}
 
 . $(dirname $0)/../functions.sh
 
-pkg_make_target()
-{
-    AR=${CROSS_PREFIX}ar            \
-    RANLIB=${CROSS_PREFIX}ranlib    \
-    CC=${CROSS_PREFIX}gcc           \
-    CFLAGS=${GLOBAL_CFLAGS}         \
-    make ${MAKEOPTS} libbz2.a || return 1
-
-    install -m644 libbz2.a ${PREFIX}/lib/libbz2.a
-    install -m644 bzlib.h ${PREFIX}/include/bzlib.h
-}
+CMAKE=1
 
 pkg_build && pkg_clean
