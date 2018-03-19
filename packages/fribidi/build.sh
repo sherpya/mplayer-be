c785c31fcacdc9e7a2ad82ca055e24cb3f518c50
sherpya <sherpya@netfarm.it>
2018-03-19 23:55:47 +0100

    packages: updated fribidi to 1.0.2

--- packages/fribidi/build.sh
+++ packages/fribidi/build.sh
@@ -4,13 +4,13 @@
 # See LICENSE for licensing informations
 
 PACKAGE=fribidi
-VERSION=1.0.1
+VERSION=1.0.2
 EXT=tar.bz2
 BASEURL=https://github.com/fribidi/fribidi/releases/download/v${VERSION}
 
 . $(dirname $0)/../functions.sh
 
 STATICLIBS="libfribidi"
-CONFOPTS="--with-glib=no"
+CONFOPTS="--disable-docs"
 
 pkg_build && pkg_clean
