a9005a8d7a0657977ab8f7b12c05a279ce44247a
sherpya <sherpya@netfarm.it>
2018-05-17 00:36:14 +0200

    packages: updated libdca to 0.0.6

--- packages/libdca/build.sh
+++ packages/libdca/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Build script for libdca free DTS Decoder
-# Copyright (c) 2013 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2018 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=libdca
-VERSION=0.0.5
+VERSION=0.0.6
 EXT=tar.bz2
 BASEURL=http://download.videolan.org/pub/videolan/${PACKAGE}/${VERSION}
 
 . $(dirname $0)/../functions.sh
 
-CFLAGS="-D__NO_INLINE__"
+#CFLAGS="-D__NO_INLINE__"
 STATICLIBS="libdca"
 
 pkg_build && pkg_clean
