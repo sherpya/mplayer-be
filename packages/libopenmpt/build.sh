bdbc124c66aae2c010eaaf6173b3de05c7d642d3
sherpya <sherpya@netfarm.it>
2018-09-03 04:05:52 +0200

    packages: updated libopenmpt to version 0.3.11

--- packages/libopenmpt/build.sh
+++ packages/libopenmpt/build.sh
@@ -4,7 +4,7 @@
 # See LICENSE for licensing informations
 
 PACKAGE=libopenmpt
-VERSION=0.3.9+release.autotools
+VERSION=0.3.11+release.autotools
 EXT=tar.gz
 BASEURL=https://lib.openmpt.org/files/libopenmpt/src
 
