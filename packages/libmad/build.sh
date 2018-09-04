4fd43759cbcedf0b15f0ff406062b6c8ab1e98d4
sherpya <sherpya@netfarm.it>
2018-09-05 00:29:08 +0200

    packages: updated libmad cve infos

--- packages/libmad/build.sh
+++ packages/libmad/build.sh
@@ -3,6 +3,11 @@
 # Copyright (c) 2013 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
+# includes fixes for:
+# CVE-2017-8372, CVE-2017-8373: 01_debian_md-size.diff
+# CVE-2017-8374: 02_debian_length-check.diff
+# 
+
 PACKAGE=libmad
 VERSION=0.15.1b
 EXT=tar.gz
