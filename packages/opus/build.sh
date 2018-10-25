242e2cb92431422bb7bcd30bb8c142dbc2d48bbf
sherpya <sherpya@netfarm.it>
2018-10-25 04:55:02 +0200

    packages: updated opus to 1.3

--- packages/opus/build.sh
+++ packages/opus/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Build script for Opus Audio Codec
-# Copyright (c) 2013-2017 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2018 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=opus
-VERSION=1.2.1
+VERSION=1.3
 EXT=tar.gz
 BASEURL=http://downloads.xiph.org/releases/${PACKAGE}
 
