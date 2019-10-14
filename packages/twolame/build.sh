0eae78acda2e47368b01b1bccdf5121ae229f1c9
Gianluigi Tiesi <sherpya@netfarm.it>
2019-10-14 19:20:52 +0200

    packages: updated twolame to 0.4.0

--- packages/twolame/build.sh
+++ packages/twolame/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Build script for TwoLAME - MPEG Audio Layer 2 Encoder
-# Copyright (c) 2013 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2019 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=twolame
-VERSION=0.3.13
+VERSION=0.4.0
 EXT=tar.gz
 BASEURL=http://downloads.sourceforge.net/project/${PACKAGE}/${PACKAGE}/${VERSION}
 
