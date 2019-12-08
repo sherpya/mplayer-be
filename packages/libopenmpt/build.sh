61df88d6fdc00335a93faec000e8f0bfc183f026
Gianluigi Tiesi <sherpya@netfarm.it>
2019-12-08 04:01:38 +0100

    packages: updated libopenmpt to 0.4.10

--- packages/libopenmpt/build.sh
+++ packages/libopenmpt/build.sh
@@ -4,7 +4,7 @@
 # See LICENSE for licensing informations
 
 PACKAGE=libopenmpt
-VERSION=0.4.9+release.autotools
+VERSION=0.4.10+release.autotools
 EXT=tar.gz
 BASEURL=https://lib.openmpt.org/files/libopenmpt/src
 
