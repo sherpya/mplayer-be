a2de964845d03c38189915a0f7894d3ce9da5a7c
Gianluigi Tiesi <sherpya@netfarm.it>
2020-04-19 16:18:05 +0200

    packages: updated gme to 0.6.3

--- packages/gme/build.sh
+++ packages/gme/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Build script for game-music-emu
-# Copyright (c) 2014-2018 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2014-2020 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=game-music-emu
-VERSION=0.6.2
+VERSION=0.6.3
 EXT=tar.xz
 BASEURL=https://bitbucket.org/mpyne/${PACKAGE}/downloads
 
