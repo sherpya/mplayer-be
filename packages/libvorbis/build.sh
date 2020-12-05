eeb9d9cbb3def753c24347d67249255df9cccd9e
Gianluigi Tiesi <sherpya@netfarm.it>
2020-12-05 19:39:02 +0100

    packages: updated libvorbis to 1.3.7

--- packages/libvorbis/build.sh
+++ packages/libvorbis/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Build script for Ogg Vorbis Audio Codec
-# Copyright (c) 2013-2019 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2020 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 # includes fixes for CVE-2017-14160 CVE-2018-5146
 
 PACKAGE=libvorbis
-VERSION=1.3.6
+VERSION=1.3.7
 EXT=tar.xz
 BASEURL=http://downloads.xiph.org/releases/vorbis
 
