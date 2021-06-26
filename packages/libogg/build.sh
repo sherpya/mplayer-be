dee7ca25e62777247f3396fe7831cb8af189d672
Gianluigi Tiesi <sherpya@netfarm.it>
2021-06-26 04:57:29 +0200

    packages: updated libogg to 1.3.5

--- packages/libogg/build.sh
+++ packages/libogg/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Build script for OGG Open Container Format
-# Copyright (c) 2013-2019 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2021 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=libogg
-VERSION=1.3.4
+VERSION=1.3.5
 EXT=tar.xz
 BASEURL=http://downloads.xiph.org/releases/ogg
 
