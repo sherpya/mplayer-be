0d7af778ad2570f783dd54516506dbf3a74b0fe4
Gianluigi Tiesi <sherpya@netfarm.it>
2021-06-26 07:18:40 +0200

    packages: libopenmpt compile the 32bit version winxp compatible

--- packages/libopenmpt/build.sh
+++ packages/libopenmpt/build.sh
@@ -18,4 +18,6 @@ CONFOPTS="--disable-openmpt123 --disable-examples --disable-tests \
           --without-mpg123 --without-ogg --without-vorbis --without-vorbisfile \
           --without-portaudio --without-portaudiocpp --without-sndfile --without-flac"
 
+[ "${ARCH}" = "x86" ] && add_to_flags "-DWINVER=_WIN32_WINNT_WIN6"
+
 pkg_build && pkg_clean
