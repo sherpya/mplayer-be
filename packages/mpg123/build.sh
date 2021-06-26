248d84c9c3120ea852499a7455f4db3c114b699f
Gianluigi Tiesi <sherpya@netfarm.it>
2021-06-26 05:11:37 +0200

    packages: updated mpg123 to 1.28.0

--- packages/mpg123/build.sh
+++ packages/mpg123/build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # Build script for libmpg123
-# Copyright (c) 2018-2020 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2018-2021 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=mpg123
-VERSION=1.26.3
+VERSION=1.28.0
 EXT=tar.bz2
 BASEURL=http://downloads.sourceforge.net/project/${PACKAGE}/${PACKAGE}/${VERSION}
 
 . $(dirname $0)/../functions.sh
 
 STATICLIBS="libmpg123 libout123"
-CONFOPTS="--enable-yasm"
+
+# configure: error: Yasm for AVX is currently broken and might go away.
+# CONFOPTS="--enable-yasm"
 
 pkg_build && pkg_clean
