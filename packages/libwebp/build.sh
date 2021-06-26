f782599e2adcc9d664ed6e0091a9825ff68589f1
Gianluigi Tiesi <sherpya@netfarm.it>
2021-06-26 05:20:42 +0200

    packages: updated libwebp to 1.2.0

--- packages/libwebp/build.sh
+++ packages/libwebp/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Build script for libwebp
-# Copyright (c) 2014-2020 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2014-2021 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=libwebp
-VERSION=1.1.0
+VERSION=1.2.0
 EXT=tar.gz
 BASEURL=https://storage.googleapis.com/downloads.webmproject.org/releases/webp
 
