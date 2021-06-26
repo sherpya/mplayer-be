8ca6d9088bc606ce4429f2b742704d64a1f8e4d5
Gianluigi Tiesi <sherpya@netfarm.it>
2021-06-26 04:46:53 +0200

    packages: updated expat to 2.4.1

--- packages/expat/build.sh
+++ packages/expat/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Build script for Expat XML Parser
-# Copyright (c) 2013-2019 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2021 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=expat
-VERSION=2.2.10
+VERSION=2.4.1
 EXT=tar.bz2
 BASEURL=http://downloads.sourceforge.net/project/${PACKAGE}/${PACKAGE}/${VERSION}
 
