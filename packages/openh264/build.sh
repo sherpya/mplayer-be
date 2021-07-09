f7dec570da1c27a27cac12c5471995cef9530aa7
Gianluigi Tiesi <sherpya@netfarm.it>
2021-07-10 01:46:15 +0200

    packages: openh264 - add Libs.private to pc
    instead of generating ld script

--- packages/openh264/build.sh
+++ packages/openh264/build.sh
@@ -11,12 +11,6 @@ __USE_MINGW_ANSI_STDIO=1
 . $(dirname $0)/../functions.sh
 
 BUILDDIR=openh264
-
 CMAKE=1
-
-post_install_hook()
-{
-    gen_ld_script libopenh264.a "-lstdc++ -lpthread"
-}
 
 git_clean && pkg_build && git_clean
