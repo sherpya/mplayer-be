43412745f4df65846db97615b20a2d8c38ae1486
Gianluigi Tiesi <sherpya@netfarm.it>
2021-10-24 12:36:31 +0200

    packages: Updated mpg123 to 1.28.1

--- packages/mpg123/build.sh
+++ packages/mpg123/build.sh
@@ -4,7 +4,7 @@
 # See LICENSE for licensing informations
 
 PACKAGE=mpg123
-VERSION=1.28.1
+VERSION=1.29.2
 EXT=tar.bz2
 BASEURL=http://downloads.sourceforge.net/project/${PACKAGE}/${PACKAGE}/${VERSION}
 
