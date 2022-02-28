3f18cff1d517c8738850497d3304c22e72185bbe
Gianluigi Tiesi <sherpya@netfarm.it>
2022-02-28 02:47:41 +0100

    packages: updated flac to 1.3.4

--- packages/flac/build.sh
+++ packages/flac/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Build script for Free Lossless Audio Codec
-# Copyright (c) 2013-2019 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2022 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 # includes fixes for: CVE-2014-8962, CVE-2014-9028
 
 PACKAGE=flac
-VERSION=1.3.3
+VERSION=1.3.4
 EXT=tar.xz
 BASEURL=http://downloads.xiph.org/releases/${PACKAGE}
 
