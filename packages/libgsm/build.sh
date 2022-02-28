08eaee6c58f30f4cf8480e439b121fe1245ed830
Gianluigi Tiesi <sherpya@netfarm.it>
2022-02-28 03:11:53 +0100

    packages: updated libgsm to 1.0.19

--- packages/libgsm/build.sh
+++ packages/libgsm/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # Build script for GSM speech compressor
-# Copyright (c) 2013-2018 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2022 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=gsm
-VERSION=1.0-pl13
-EXT=tar.xz
-BASEURL=local
+VERSION=1.0.19
+EXT=tar.gz
+BASEURL=http://www.quut.com/gsm
 
 . $(dirname $0)/../functions.sh
 
 CMAKE=1
+BUILDDIR="gsm-1.0-pl19"
 
 pkg_build && pkg_clean
