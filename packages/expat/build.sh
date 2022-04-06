292758356110ab706190005b5239abd0c42a7bbe
Gianluigi Tiesi <sherpya@netfarm.it>
2022-04-07 00:48:03 +0200

    packages: updated expat to 2.4.8

--- packages/expat/build.sh
+++ packages/expat/build.sh
@@ -4,7 +4,7 @@
 # See LICENSE for licensing informations
 
 PACKAGE=expat
-VERSION=2.4.6
+VERSION=2.4.8
 EXT=tar.bz2
 BASEURL=http://downloads.sourceforge.net/project/${PACKAGE}/${PACKAGE}/${VERSION}
 
