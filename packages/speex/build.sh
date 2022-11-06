9df7bff2c386fb49e3c5423395a9931281c6b2b9
Gianluigi Tiesi <sherpya@netfarm.it>
2022-11-06 02:48:26 +0100

    Updated speex to 1.2.1

--- packages/speex/build.sh
+++ packages/speex/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Build script for Speex Audio Codec
-# Copyright (c) 2013-2018 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2022 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=speex
-VERSION=1.2.0
+VERSION=1.2.1
 EXT=tar.gz
 BASEURL=http://downloads.xiph.org/releases/${PACKAGE}
 
