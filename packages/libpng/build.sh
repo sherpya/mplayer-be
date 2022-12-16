c77c84d6d08ee9a3fc93000fece067e83df26634
Gianluigi Tiesi <sherpya@netfarm.it>
2022-12-16 23:40:16 +0100

    packages: updated libpng to 1.6.39

--- packages/libpng/build.sh
+++ packages/libpng/build.sh
@@ -4,7 +4,7 @@
 # See LICENSE for licensing informations
 
 PACKAGE=libpng
-VERSION=1.6.38
+VERSION=1.6.39
 EXT=tar.xz
 BASEURL=http://downloads.sourceforge.net/project/${PACKAGE}/libpng16/${VERSION}
 
