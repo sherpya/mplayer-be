a1fc514994fe5fe15a539117e70540df53308ff1
Gianluigi Tiesi <sherpya@gmail.com>
2024-03-27 03:22:39 +0100

    packages: updated opus to version 1.5.1

--- packages/opus/build.sh
+++ packages/opus/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # Build script for Opus Audio Codec
-# Copyright (c) 2013-2019 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2024 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=opus
-VERSION=1.3.1
+VERSION=1.5.1
 EXT=tar.gz
 BASEURL=http://downloads.xiph.org/releases/${PACKAGE}
 
 . $(dirname $0)/../functions.sh
 
+CFLAGS="-Wall -O2"
 STATICLIBS="libopus"
 CONFOPTS="--disable-doc --disable-extra-programs"
 
