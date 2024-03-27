9ef68453aea31218ca4d5e7c3c04bb788e8ee548
Gianluigi Tiesi <sherpya@gmail.com>
2024-03-27 01:10:37 +0100

    packages: updated freetype to version 2.13.2

--- packages/freetype/build.sh
+++ packages/freetype/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Build script for FreeType library
-# Copyright (c) 2013-2022 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2013-2024 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=freetype
-VERSION=2.12.1
+VERSION=2.13.2
 EXT=tar.gz
 BASEURL=http://downloads.sourceforge.net/project/${PACKAGE}/freetype2/${VERSION}
 
