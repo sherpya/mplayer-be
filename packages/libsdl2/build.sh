3bb1d1dd7d5da5c2eaa61b214cec655a5b7caf04
Gianluigi Tiesi <sherpya@gmail.com>
2025-02-17 19:22:21 +0100

    packages: libsdl2 -> update to 2.32.0

--- packages/libsdl2/build.sh
+++ packages/libsdl2/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Build script for Simple DirectMedia Layer 2 (SDL)
-# Copyright (c) 2019-2024 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2019-2025 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=SDL2
-VERSION=2.30.1
+VERSION=2.32.0
 EXT=tar.gz
 BASEURL=http://www.libsdl.org/release
 
