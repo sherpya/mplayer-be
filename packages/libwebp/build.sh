312c8f8b41f5ced3493eb3000ef28b654a99f979
Gianluigi Tiesi <sherpya@gmail.com>
2025-02-17 19:22:58 +0100

    packages: libwebp -> update to 1.5.0

--- packages/libwebp/build.sh
+++ packages/libwebp/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Build script for libwebp
-# Copyright (c) 2014-2024 Gianluigi Tiesi <sherpya@netfarm.it>
+# Copyright (c) 2014-2025 Gianluigi Tiesi <sherpya@netfarm.it>
 # See LICENSE for licensing informations
 
 PACKAGE=libwebp
-VERSION=1.3.2
+VERSION=1.5.0
 EXT=tar.gz
 BASEURL=https://storage.googleapis.com/downloads.webmproject.org/releases/webp
 
