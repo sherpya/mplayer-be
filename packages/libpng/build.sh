13798605cb0d0ef3467d8c6ac42ab0200fb64b7b
Gianluigi Tiesi <sherpya@gmail.com>
2025-03-09 01:08:12 +0100

    packages: libpng -> 1.6.47

--- packages/libpng/build.sh
+++ packages/libpng/build.sh
@@ -4,7 +4,7 @@
 # See LICENSE for licensing informations
 
 PACKAGE=libpng
-VERSION=1.6.46
+VERSION=1.6.47
 EXT=tar.xz
 BASEURL=http://downloads.sourceforge.net/project/${PACKAGE}/libpng16/${VERSION}
 
